f6fb9b489911c9bc6ede7e886d96d2d9afbb2e46
GHauer <hauer.georg@gmail.com>
2014-05-19 12:10:03 -0500

    2nd route - "get"

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rxngif::Application.routes.draw do
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
 end
+
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
