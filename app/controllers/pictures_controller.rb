4a36f299cc1462d467debe9201262039cdfb3e5e
GHauer <hauer.georg@gmail.com>
2014-05-19 12:12:28 -0500

    Def index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,11 @@
 class PicturesController < ApplicationController
+
   def show
-
     @picture = Picture.find_by({ :id => params[:id] })
-
   end
+
+  def index
+    @list_of_pictures = Picture.all
+  end
+
 end
