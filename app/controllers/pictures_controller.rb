8fab41086aaf6dcbc2940c16821bfaf32457946b
GHauer <hauer.georg@gmail.com>
2014-05-19 13:22:51 -0500

    "New" > "Create" link

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,20 +8,37 @@ class PicturesController < ApplicationController
     @list_of_pictures = Picture.all
   end
 
+  def new
 
-def create
+  end
+
+  def create
     @picture=Picture.new
     @picture.caption= params[:caption]
     @picture.source= params[:source]
     @picture.save
+
+    redirect_to("http://localhost:3000/create_picture")
+
   end
+
+
+
+
+
+
+
+
+
+
+
 
 #   def update
 #     @picture=Picture.find_by({:id => params[:id]})
 #     @picture.caption= params[:caption]
 #     @picture.source= params[:source]
 #     @picture.save
-#   end=end
+#   end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rxngif::Application.routes.draw do
   get("/all_pictures", { :controller => "pictures", :action => "index" })
 
   get("/new_picture_form", { :controller => "pictures", :action => "new" })
+
+  get("/create_picture", { :controller => "pictures", :action => "create" })
+
+
 end
